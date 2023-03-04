b5afd022ae40eab55487f63eaf5b80014f0b36dc
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-04 09:27:10 -0300

    Foi criado um função para cada as 4 operações matematicas básicas

--- CalculadoraSimples/Program.cs
+++ CalculadoraSimples/Program.cs
@@ -1,59 +1,105 @@
-﻿double num1, num2;
-int opcao = 0;
-
-void Menu()
+﻿
+internal class Program
 {
-    Console.Clear();
-    Console.WriteLine("1 - Somar");
-    Console.WriteLine("2 - Subtrair");
-    Console.WriteLine("3 - Multiplicar");
-    Console.WriteLine("4 - Dividir");
-    Console.WriteLine("5 - Sair");
-    Console.WriteLine("Escolha uma opção: ");
-    opcao = int.Parse(Console.ReadLine());
-
-    switch(opcao)
+    private static void Main(string[] args)
     {
-        default:
-            Console.WriteLine("Opção Inválida !");
-            Console.ReadLine();
-        break;
+        int opcao = 0;
+        int numero1 = 0, numero2 = 0;
 
-        case 1:
-            Console.WriteLine("A soma dos números é: " + (num1 + num2));
-            Console.ReadLine();
-        break;
+        int Menu()
+        {
+            Console.WriteLine("1 - Somar dois números");
+            Console.WriteLine("2 - Subtrair dois números");
+            Console.WriteLine("3 - Multiplicar dois números");
+            Console.WriteLine("4 - Dividir dois núemros");
+            Console.WriteLine("5 - sair");
+            opcao = int.Parse(Console.ReadLine());
 
-        case 2:
-            Console.WriteLine("A subtração é : " +(num1 - num2));
-            Console.ReadLine();
-        break;
+            return opcao;
+        }
 
-        case 3:
-            Console.WriteLine("A Multiplicação  é : " + (num1 * num2));
-            Console.ReadLine();
-        break;
+        void Somar(int numero1, int numero2)
+        {
+            Console.WriteLine("A soma entre o " + numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+        }
+        void Subtrair(int numero1, int numero2)
+        {
+            Console.WriteLine("A subtração entre o " + numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+        }
+        void Multiplicacao(int numero1, int numero2)
+        {
+            Console.WriteLine("A multiplicação entre o " + numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+        }
+        void Divisao(int numero1, int numero2)
+        {
+            if (numero2 == 0)
+            {
+                Console.WriteLine("Não é possivel dividir por zero");
+            }
+            else
+            {
+                double divisao = (double)numero1 / numero2;
+                Console.WriteLine("A divisão entre o " + numero1 + " / " + numero2 + " = " + divisao.ToString("F2"));
+            }
+        }
+        void MensagemAteLogo()
+        {
+            Console.WriteLine("Até logo !!!");
+        }
+        void OpcaoInvalida()
+        {
+            Console.WriteLine("Opção inválida !!!");
+        }
 
-        case 4:
-            Console.WriteLine("A divisão é :" + (num1 / num2));
-            Console.ReadLine();
-        break;
+        void escolha(int opcao, int numero1, int numero2)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Somar(numero1, numero2);
+                    break;
 
-        case 5:
-            Console.WriteLine("Até logo !");
-            Console.ReadLine();
-        break;
+                case 2:
+                    Subtrair(numero1, numero2);
+                    break;
+
+                case 3:
+                    Multiplicacao(numero1, numero2);
+                    break;
+
+                case 4:
+                    Divisao(numero1, numero2);
+                    break;
+
+                case 5:
+                    MensagemAteLogo();
+                    break;
+
+                default:
+                    OpcaoInvalida();
+                    break;
+
+            }
+        }
+
+        int PegarNumero(int i)
+        {
+            int numero;
+            Console.WriteLine("Digite o " + i + "º número: ");
+            numero = int.Parse(Console.ReadLine());
+            return numero;
+        }
+
+        numero1 = PegarNumero(1);
+        numero2 = PegarNumero(2);
+
+        while (opcao != 5)
+        {
+
+            opcao = Menu();
+            escolha(opcao, numero1, numero2);
+
+        }
+
     }
-}
-
-
-Console.WriteLine("Informe o primeiro número");
-num1 = int.Parse(Console.ReadLine());
-
-Console.WriteLine("Informe o segundo número");
-num2 = int.Parse(Console.ReadLine());   
-
-while(opcao != 5)
-{
-    Menu();
 }
